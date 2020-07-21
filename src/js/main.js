e430279119ad7b39f95887ccd8928c31bd882ced
katylorny <katy_lorny@mail.ru>
2020-07-21 23:18:13 +0300

    слайдеры на главной странице

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,42 @@
 import $ from '../local_modules/jquery/dist/jquery.min'
+import Swiper from "swiper"
 
 $(document).ready(() => {
     // eslint-disable-next-line no-console
     console.log(`document ready`)
+})
+
+$(`.header__menu-open-button`).click(function () {
+    $(`.header`).toggleClass(`header--menu-opened header--white`)
+})
+
+const swiper = new Swiper(`.swiper-container`, {
+    slidesPerView: `auto`,
+    spaceBetween: 20,
+    centeredSlides: true,
+    pagination: {
+        el: `.swiper-pagination`,
+        clickable: true,
+    },
+    breakpoints: {
+        // when window width is >= 640px
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+            centeredSlides: false,
+        }
+    }
+})
+
+
+const swiperClients = new Swiper(`.swiper-container-clients`, {
+    slidesPerView: `auto`,
+    spaceBetween: 20,
+    breakpoints: {
+    // when window width is >= 640px
+        768: {
+            slidesPerView: 5,
+            // spaceBetween: 40,
+        }
+    }
 })
