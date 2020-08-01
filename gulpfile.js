5517bd82b68633bbc78069959a57a132adb1344c
katylorny <katy_lorny@mail.ru>
2020-08-02 01:18:33 +0300

    видео и кнопка

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ const clean = require('./gulp/tasks/clean')
 const copyDependencies = require('./gulp/tasks/copyDependencies')
 const lighthouse = require('./gulp/tasks/lighthouse')
 const svgSprite = require('./gulp/tasks/svgSprite')
+const video = require('./gulp/tasks/video')
 
 function setMode(isProduction = false) {
   return cb => {
@@ -18,7 +19,7 @@ function setMode(isProduction = false) {
   }
 }
 
-const dev = gulp.parallel(pug2html, styles, script, fonts, imageMinify, svgSprite)
+const dev = gulp.parallel(pug2html, styles, script, fonts, imageMinify, video, svgSprite)
 
 const build = gulp.series(clean, copyDependencies, dev)
 
